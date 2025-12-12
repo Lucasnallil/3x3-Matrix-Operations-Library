c2282b5548918775ed7c005d17d4731dedaf0641
林彥均 <lucas951116@gmail.com>
2025-12-12 16:00:34 +0800

    Initial Commit

---
+++ 程式設計期末作業/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  程式設計期末作業
+//
+//  Created by 林彥均 on 2025/12/12.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
